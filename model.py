dbcdb725baac686882ad3dcf40b788931753a704
Jasman-py <jasmanbirsinghmh@gmail.com>
2025-09-19 14:13:52 +0530

    created

---
+++ model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+import PIL
+
+class Model:
+    #def __init__(self):
+        #self.model=keras.models.load_model('Camera Classifier')
+
+    def train_model(self,counters):
+
